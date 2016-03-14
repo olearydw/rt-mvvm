1b3033e5200b68bbc0bb7b3d226455552f039a61
Dan O'Leary <doleary@esri.com>
2016-03-13 18:28:17 -0700

    initial commit

---
+++ rt-mvvm/DelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace rt_mvvm
+{
+    class DelegateCommand : System.Windows.Input.ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<object> executeAction, Func<object, bool> canExecuteFunc = null)
+        {
+            this.canExecute = canExecuteFunc;
+            this.execute = executeAction;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecute == null) { return true;  }
+            return this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
---
+++ rt-mvvm/MapViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Controls;
+using Esri.ArcGISRuntime.Layers;
+
+namespace rt_mvvm
+{
+    class MapViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaiseNotifyPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        public void MyMapViewExtentChanged(object parameter)
+        {
+            var mv = parameter as MapView;
+            var extent = mv.Extent;
+            //CurrentExtentString = "FOO BAR";
+            //Console.Write(extent);
+            CurrentExtentString = string.Format("XMin={0:F2} YMin={1:F2} XMax={2:F2} YMax={3:F2}", 
+            extent.XMin, extent.YMin, extent.XMax, extent.YMax);
+        }
+
+
+        private Map map;
+        public Map IncidentMap
+        {
+            get { return this.map; }
+            set { this.map = value; }
+        }
+
+        private string extentString;
+        public string CurrentExtentString
+        {
+            get
+            {
+                return this.extentString;
+            }
+            set
+            {
+                this.extentString = value;
+                this.RaiseNotifyPropertyChanged();
+            }
+        }
+
+        
+
+        public MapViewModel()
+        {
+            this.map = App.Current.Resources["IncidentMap"] as Map;
+            ToggleLayerCommand = new DelegateCommand(ToggleLayer, OkToExecute);
+            ExtentChangedCommand = new DelegateCommand(MyMapViewExtentChanged);
+
+        }
+
+        public DelegateCommand ToggleLayerCommand { get; set; }
+        public DelegateCommand ExtentChangedCommand { get; set; }
+
+        private void ToggleLayer(object parameter)
+        {
+            var lyr = this.map.Layers[parameter.ToString()];
+            lyr.IsVisible = !(lyr.IsVisible);
+        }
+
+        private bool OkToExecute(object parameter)
+        {
+            var lyr = this.map.Layers[parameter.ToString()] as FeatureLayer;
+            return (lyr != null);
+        }
+
+        
+ 
+    }
+}
